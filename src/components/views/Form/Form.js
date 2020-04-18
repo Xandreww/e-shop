e3b46162f18d363d1f4ee13e0925500cbb00f438
Andrzej Popek <andrzejpopek93@gmail.com>
2020-04-18 13:53:48 +0200

    ehance form sketch

--- src/components/views/Form/Form.js
+++ src/components/views/Form/Form.js
@@ -16,9 +16,17 @@ class Component extends React.Component {
     // const { products } = this.props;
 
     return (
-      <Form>
+      <Form className={styles.root}>
         <Form.Group controlId="formBasicEmail">
-          <Form.Label>Email address</Form.Label>
+          <Form.Label>First name, last name</Form.Label>
+          <Form.Control type="text" placeholder="John Doe" />
+        </Form.Group>
+        <Form.Group controlId="formBasicEmail">
+          <Form.Label>Address</Form.Label>
+          <Form.Control type="text" placeholder="Your address" />
+        </Form.Group>
+        <Form.Group controlId="formBasicEmail">
+          <Form.Label>Email</Form.Label>
           <Form.Control type="email" placeholder="Enter email" />
         </Form.Group>
         <Form.Group controlId="exampleForm.SelectCustom">
@@ -37,13 +45,13 @@ class Component extends React.Component {
           </Form.Control>
         </Form.Group>
         <Form.Group controlId="exampleForm.ControlTextarea1">
-          <Form.Label>Example textarea</Form.Label>
+          <Form.Label>Additional remarks:</Form.Label>
           <Form.Control as="textarea" rows="3" />
         </Form.Group>
         <Form.Group controlId="formBasicCheckbox">
           <Form.Check type="checkbox" label="I accept terms and conditions" />
         </Form.Group>
-        <Button variant="primary" type="submit">
+        <Button variant="primary" type="submit" className={styles.submitButton}>
           Submit
         </Button>
       </Form>
