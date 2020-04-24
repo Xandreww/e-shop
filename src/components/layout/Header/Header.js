85d3b8276b742cf79315e4207f49fe413bcf0c2b
Andrzej Popek <andrzejpopek93@gmail.com>
2020-04-24 17:14:07 +0200

    cart icon now shows real products number

--- src/components/layout/Header/Header.js
+++ src/components/layout/Header/Header.js
@@ -6,52 +6,62 @@ import { IoIosArrowDown } from 'react-icons/io';
 import profileImage from '../../../images/profilePhoto.png';
 import { Link } from 'react-router-dom';
 
-// import { connect } from 'react-redux';
-// import { reduxSelector, reduxActionCreator } from '../../../redux/exampleRedux.js';
+import { connect } from 'react-redux';
+import { getCart } from '../../../redux/productsRedux';
 
 import styles from './Header.module.scss';
 
-const Component = ({ children }) => (
-  <Navbar bg="light" expand="lg">
-    <div className={styles.navbar}>
-      <Navbar.Brand as={Link} to="/" className={styles.brand}>
-        <p>E-shop</p>
-      </Navbar.Brand>
-      <Form inline>
-        <FormControl type="text" placeholder="Hello, what are you looking for?" className={styles.search} />
-      </Form>
-      <div className={styles.rightSide}>
-        <Button as={Link} to="/Cart" className={styles.cartButton}>
-          <FiShoppingCart className={styles.cartIcon} />
-          <p className={styles.number}>10</p>
-        </Button>
-        <Button className={styles.profileButton}>
-          <img src={profileImage} alt="profile" />
-          <p>Hi, John!</p>
-          <IoIosArrowDown className={styles.arrowIcon} />
-        </Button>
-      </div>
-    </div>
-  </Navbar>
-);
+class Component extends React.Component {
+  static propTypes = {
+    products: PropTypes.array,
+  };
 
-Component.propTypes = {
-  children: PropTypes.node,
-  className: PropTypes.string,
-};
+  numberOfProducts = () => {
+    const { products } = this.props;
 
-// const mapStateToProps = state => ({
-//   someProp: reduxSelector(state),
-// });
+    if (products.length > 0) {
+      return products.length;
+    } else {
+      return null;
+    }
+  };
 
-// const mapDispatchToProps = dispatch => ({
-//   someAction: arg => dispatch(reduxActionCreator(arg)),
-// });
+  render() {
+    const { numberOfProducts } = this;
+    return (
+      <Navbar bg="light" expand="lg">
+        <div className={styles.navbar}>
+          <Navbar.Brand as={Link} to="/" className={styles.brand}>
+            <p>E-shop</p>
+          </Navbar.Brand>
+          <Form inline>
+            <FormControl type="text" placeholder="Hello, what are you looking for?" className={styles.search} />
+          </Form>
+          <div className={styles.rightSide}>
+            <Button as={Link} to="/Cart" className={styles.cartButton}>
+              <FiShoppingCart className={styles.cartIcon} />
+              <p className={styles.number}>{numberOfProducts()}</p>
+            </Button>
+            <Button className={styles.profileButton}>
+              <img src={profileImage} alt="profile" />
+              <p>Hi, John!</p>
+              <IoIosArrowDown className={styles.arrowIcon} />
+            </Button>
+          </div>
+        </div>
+      </Navbar>
+    );
+  }
+}
 
-// const Container = connect(mapStateToProps, mapDispatchToProps)(Component);
+const mapStateToProps = (state) => ({
+  products: getCart(state),
+});
+
+const Container = connect(mapStateToProps)(Component);
 
 export {
-  Component as Header,
-  // Container as Header,
+  // Component as Header,
+  Container as Header,
   Component as HeaderComponent,
 };
