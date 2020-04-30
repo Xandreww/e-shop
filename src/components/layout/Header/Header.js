49d739c0c3a43a19e84308c3cf7bc1b9374086e0
Andrzej Popek <andrzejpopek93@gmail.com>
2020-04-30 16:01:40 +0200

    add header RWD

--- src/components/layout/Header/Header.js
+++ src/components/layout/Header/Header.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import { Navbar, Form, FormControl, Button } from 'react-bootstrap';
 import { FiShoppingCart } from 'react-icons/fi';
 import { IoIosArrowDown } from 'react-icons/io';
+import { GoSearch } from 'react-icons/go';
 import profileImage from '../../../images/profilePhoto.png';
 import { Link } from 'react-router-dom';
 
@@ -12,9 +13,17 @@ import { getCart } from '../../../redux/productsRedux';
 import styles from './Header.module.scss';
 
 class Component extends React.Component {
+  state = {
+    viewportWidth: 0,
+  };
+
   static propTypes = {
     products: PropTypes.array,
   };
+
+  componentWillMount() {
+    this.setState({ viewportWidth: window.innerWidth });
+  }
 
   numberOfProducts = () => {
     const { products } = this.props;
@@ -28,15 +37,22 @@ class Component extends React.Component {
 
   render() {
     const { numberOfProducts } = this;
+    const { viewportWidth } = this.state;
+
     return (
       <Navbar bg="light" expand="lg">
+        {console.log('viewportWidth:', viewportWidth)}
         <div className={styles.navbar}>
           <Navbar.Brand as={Link} to="/" className={styles.brand}>
             <p>E-shop</p>
           </Navbar.Brand>
-          <Form inline>
-            <FormControl type="text" placeholder="Hello, what are you looking for?" className={styles.search} />
-          </Form>
+          {viewportWidth > 425 ? (
+            <Form inline>
+              <FormControl type="text" placeholder="Hello, what are you looking for?" className={styles.search} />
+            </Form>
+          ) : (
+            <GoSearch className={styles.searchIcon} />
+          )}
           <div className={styles.rightSide}>
             <Button as={Link} to="/Cart" className={styles.cartButton}>
               <FiShoppingCart className={styles.cartIcon} />
